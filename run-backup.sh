042be5cac0ce88a99768b1210b5cadf8344551cd
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2016-04-13 18:22:17 -0300

    working in run-backup.sh

--- run-backup.sh
+++ run-backup.sh
@@ -2,5 +2,13 @@
 
 set -e
 
-#TODO
+
 echo "Exec backup!"
+
+aws s3 ls s3://$AWS_ACCESS_KEY_ID
+
+#Testing
+s3Result=$(aws s3 ls s3://$AWS_S3_BUCKET/$AWS_S3_PATH )
+
+printf "s3Result %s \n" "$s3Result"
+
