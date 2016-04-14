4e9b2db35c22b67a2f976b2cf6a608b12f223446
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2016-04-13 21:26:45 -0300

    working in run-backup.sh

--- run-backup.sh
+++ run-backup.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+#
+#Author: frekele
+#
 
 set -e
 
+timeBegin=$(date +%Y%m%d%H%M%S)
+echo ""
+echo "timeBegin=$timeBegin"
+echo ""
 
-echo "Exec backup!"
+dateTimeUtc=$(date --utc +%FT%TZ)
+if [ "$BACKUP_NAME" ]; then
+    fileName="$BACKUP_NAME-$dateTimeUtc.tar.gz"
+else
+    fileName="$dateTimeUtc.tar.gz"
+fi
+echo "fileName=$fileName"
+echo ""
 
-aws s3 ls s3://$AWS_ACCESS_KEY_ID
+#remove slash in end url.
+AWS_S3_PATH=`echo "${AWS_S3_PATH}" | sed 's#/*$##'`
+DATA_PATH=`echo "${DATA_PATH}" | sed 's#/*$##'`
 
-#Testing
-s3Result=$(aws s3 ls s3://$AWS_S3_BUCKET/$AWS_S3_PATH )
+echo "Starting compress $DATA_PATH to /tmp/$fileName"
+tar -zcf /tmp/$fileName  -C $DATA_PATH/ .
+echo ""
 
-printf "s3Result %s \n" "$s3Result"
+echo "Starting backup from $DATA_PATH/$fileName to $AWS_S3_PATH/$fileName"
+s3Result=$(aws s3 cp /tmp/$fileName $AWS_S3_PATH/$fileName )
+echo ""
 
+echo "s3Result=$s3Result"
+echo ""
+
+rm -f /tmp/$fileName
+
+timeEnd=$(date +%Y%m%d%H%M%S)
+echo "timeEnd=$timeEnd"
+echo ""
+
+timeDuration=$((timeEnd - timeBegin))
+echo "timeDuration=$timeDuration second(s)"
+echo ""
