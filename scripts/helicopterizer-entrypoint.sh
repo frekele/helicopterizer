a09a9b44aa419b3e217be8db39a2d6bc1fdcadeb
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2016-04-15 23:57:37 -0300

    enable docker subcommand.

--- scripts/helicopterizer-entrypoint.sh
+++ scripts/helicopterizer-entrypoint.sh
@@ -4,6 +4,25 @@ set -e
 
 echo "Starting helicopterizer..."
 
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- docker "$@"
+fi
+
+# if our command is a valid Docker subcommand, let's invoke it through Docker instead
+# (this allows for "docker run docker ps", etc)
+if docker "$1" --help > /dev/null 2>&1; then
+	set -- docker "$@"
+fi
+
+# if we have "--link some-docker:docker" and not DOCKER_HOST, let's set DOCKER_HOST automatically
+if [ -z "$DOCKER_HOST" -a "$DOCKER_PORT_2375_TCP" ]; then
+	export DOCKER_HOST='tcp://docker:2375'
+fi
+
+exec "$@"
+
+
 export DATA_PATH=${DATA_PATH:-/data/}
 
 : ${AWS_ACCESS_KEY_ID:?"Environment variable AWS_ACCESS_KEY_ID is required!"}
