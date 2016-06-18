73635ff443bc11a6fa54f2e29d708addde4b901e
Luis Hdez <luis.munoz.hdez@gmail.com>
2016-06-18 19:16:27 +0200

    Fix arguments for filename in restore

--- scripts/core/tarball/restore.sh
+++ scripts/core/tarball/restore.sh
@@ -29,7 +29,7 @@ extractTarball(){
 
 
 #Call to mount file name.
-fileName=$(mountFileName $BACKUP_VERSION $GZIP_COMPRESSION $BACKUP_PREFIX $BACKUP_NAME)
+fileName=$(mountFileName "$BACKUP_VERSION" $GZIP_COMPRESSION "$BACKUP_PREFIX" "$BACKUP_NAME")
 
 
 #Call to mount Restore file name for copy to /tmp.
